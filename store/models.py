628dcc5c23cda11d1518f993c4e91ead6eb0d48e
Will Krause <wkrause1@radford.edu>
2018-10-08 17:04:12 -0400

    Initial commit

---
+++ store/models.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.db import models
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=400)
+    product_description = models.CharField(max_length=600)
+    product_price = models.FloatField(default=0.0)
